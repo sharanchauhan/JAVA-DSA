1a87b0ff81ef13195d501f9bb0ecce039a1d2175
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-24 23:22:39 +0530

    Function to display a Generic tree added

--- GenericTree/L001/Main.java
+++ GenericTree/L001/Main.java
@@ -32,8 +32,23 @@ public class Main{
         return root;
     }
     
+    public static void display(Node node) 
+    {
+        System.out.print(node.data+" -> ");
+        for (Node child : node.children) 
+        {
+            System.out.print(child.data+" ");
+        }
+        System.out.println(".");
+        for (Node child : node.children) 
+        {
+            display(child);
+        }
+    }
+
     public static void main(String args[]){
         Integer []arr = {10, 20, 50, null, 60, null, null, 30, 70, 90, null, 100, null, 110, null, null, null, 40, 80, null, null, null};
-        construct(arr);
+        Node root=construct(arr);
+        display(root);
     }
 }
