d827666d875c228f3df7a4a278c322d096053a19
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-26 02:15:29 +0530

    LevelOrder Linewise done and formated the file

--- GenericTree/L002/Main.java
+++ GenericTree/L002/Main.java
@@ -1,37 +1,56 @@
 import java.util.*;
 
 public class Main {
-  static class Node{
+  static class Node {
     int data;
     ArrayList<Node> children;
 
-    Node(int data){
-        this.data = data;
-        children = new ArrayList<>();
+    Node(int data) {
+      this.data = data;
+      children = new ArrayList<>();
     }
-}
-    public static void traversals(Node node){
-        System.out.println("Node Pre "+node.data);
-        for (Node child : node.children) {
-          System.out.println("Edge Pre "+node.data+"--"+child.data);
-          traversals(child);
-          System.out.println("Edge Post "+node.data+"--"+child.data);
-        }
-        System.out.println("Node Post "+node.data);
-      }
+  }
 
-      public static void levelOrder(Node node){
-        Queue<Node> q=new ArrayDeque<>();
-        q.add(node);
-        while(q.size()>0)
-        {
-          Node rem=q.remove();
-          System.out.print(rem.data+" ");
-          for(Node child:rem.children)
-          {
-            q.add(child);
-          }
-        }
-        System.out.println(".");
-        }
+  public static void traversals(Node node) {
+    System.out.println("Node Pre " + node.data);
+    for (Node child : node.children) {
+      System.out.println("Edge Pre " + node.data + "--" + child.data);
+      traversals(child);
+      System.out.println("Edge Post " + node.data + "--" + child.data);
+    }
+    System.out.println("Node Post " + node.data);
+  }
+
+  public static void levelOrder(Node node) {
+    Queue<Node> q = new ArrayDeque<>();
+    q.add(node);
+    while (q.size() > 0) {
+      Node rem = q.remove();
+      System.out.print(rem.data + " ");
+      for (Node child : rem.children) {
+        q.add(child);
+      }
+    }
+    System.out.println(".");
+  }
+
+  public static void levelOrderLinewise(Node node) {
+    // write your code here
+    Queue<Node> mq = new ArrayDeque<>();
+    Queue<Node> hq = new ArrayDeque<>();
+    mq.add(node);
+    while (mq.size() > 0) {
+      Node rem = mq.remove();
+      System.out.print(rem.data + " ");
+      for (Node child : rem.children) {
+        hq.add(child);
+      }
+      if (mq.size() == 0) {
+        System.out.println();
+        Queue<Node> tq = mq;
+        mq = hq;
+        hq = tq;
+      }
+    }
+  }
 }
