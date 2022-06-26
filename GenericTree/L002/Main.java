bea73a716ba03626569f0ad432ec0a0e382f6290
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-26 12:33:37 +0530

    Function to find data in a generic tree added

--- GenericTree/L002/Main.java
+++ GenericTree/L002/Main.java
@@ -53,4 +53,21 @@ public class Main {
       }
     }
   }
+
+  public static boolean find(Node node, int data) {
+    // write your code here
+    if(node.data==data)
+    {
+      return true;
+    }
+    for (Node child : node.children) 
+    {
+      boolean ans=find(child,data);
+      if(ans)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
 }
