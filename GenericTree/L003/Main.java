0207ac5d2f4e8e7d59e5d64a3a9bc043e35c4676
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-27 20:31:41 +0530

    Formated document and Function to linearize the Generic Tree added

--- GenericTree/L003/Main.java
+++ GenericTree/L003/Main.java
@@ -24,25 +24,42 @@ public class Main {
         return new ArrayList<>();
     }
 
-    public static int lca(Node node, int d1, int d2) {
-        // write your code here
-        ArrayList<Integer> path1,path2;
-        path1=nodeToRootPath(node,d1);
-        path2=nodeToRootPath(node,d2);
-        int i=path1.size()-1;
-        int j=path2.size()-1;
-        while(i>=0 && j>=0)
-        {
-          if(path1.get(i)==path2.get(j))
-          {
-            i--;
-            j--;
-          }
-          else
-          {
-            break;
-          }
+    public static int lca(Node node, int d1, int d2) 
+    {
+        ArrayList<Integer> path1, path2;
+        path1 = nodeToRootPath(node, d1);
+        path2 = nodeToRootPath(node, d2);
+        int i = path1.size() - 1;
+        int j = path2.size() - 1;
+        while (i >= 0 && j >= 0) {
+            if (path1.get(i) == path2.get(j)) {
+                i--;
+                j--;
+            } else {
+                break;
+            }
         }
-        return path1.get(i+1);
-      }
+        return path1.get(i + 1);
+    }
+
+    public static Node getTailNode(Node node) {
+        while (node.children.size() != 0) {
+            node = node.children.get(0);
+        }
+        return node;
+    }
+
+    public static void linearize(Node node) {
+        for (Node child : node.children) {
+            linearize(child);
+        }
+
+        while (node.children.size() > 1) {
+            Node lc = node.children.get(node.children.size() - 1);
+            Node slc = node.children.get(node.children.size() - 2);
+            node.children.remove(node.children.size() - 1);
+            Node tailNode = getTailNode(slc);
+            tailNode.children.add(lc);
+        }
+    }
 }
