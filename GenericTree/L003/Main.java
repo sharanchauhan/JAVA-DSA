f008c0b94231787126c29720814fd399f871a275
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-27 22:38:36 +0530

    Function to remove the leaves of the Generic Tree added

--- GenericTree/L003/Main.java
+++ GenericTree/L003/Main.java
@@ -68,4 +68,20 @@ public class Main {
         int lc = lca(node, d1, d2);
         return (p1.indexOf(lc) + p2.indexOf(lc));
     }
+
+    public static void removeLeaves(Node node) {
+        // write your code here
+        for(int i=node.children.size()-1;i>=0;i--)
+        {
+          Node child=node.children.get(i);
+          if(child.children.size()==0)
+          {
+            node.children.remove(i);
+          }
+        }
+    
+        for (Node child : node.children) {
+          removeLeaves(child);
+        }
+      }
 }
