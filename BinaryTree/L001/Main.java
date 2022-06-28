b39b52b877d0842445d2a77c6ba4720f7625dbc6
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-28 09:39:33 +0530

    Binary Tree Started and Structure for Binary tree created in Main.java file

---
+++ BinaryTree/L001/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static class Node
+    {
+        int data;
+        Node left,right;
+        Node(int val)
+        {
+            this.data=val;
+        }
+    }
+    public static class Pair
+    {
+        Node node;
+        int state;
+        Pair(Node node)
+        {
+            this.node=node;
+            this.state=0;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
