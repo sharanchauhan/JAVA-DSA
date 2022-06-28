fba52e26787f8c0c61839f2882d67eb69423b117
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-28 10:23:39 +0530

    Function to create or construct a Binary tree added in Main.java file

--- BinaryTree/L001/Main.java
+++ BinaryTree/L001/Main.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Main {
     public static class Node
     {
@@ -17,6 +19,46 @@ public class Main {
             this.node=node;
             this.state=0;
         }
+    }
+    public static Node construct(Integer arr[])
+    {
+        Node root=new Node(arr[0]);
+        Stack<Pair> st=new Stack<>();
+        st.push(new Pair(root));
+        int idx=1;
+        while(st.size()>0)
+        {
+            Pair top=st.peek();
+            if(top.state==0)
+            {
+                Integer val=arr[idx];
+                if(val!=null)
+                {
+                    Node child=new Node(val);
+                    top.node.left=child;
+                    st.push(new Pair(child));
+                }
+                top.state++;
+                idx++;
+            }
+            else if(top.state==1)
+            {
+                Integer val=arr[idx];
+                if(val!=null)
+                {
+                    Node child=new Node(val);
+                    top.node.right=child;
+                    st.push(new Pair(child));
+                }
+                top.state++;
+                idx++;
+            }
+            else if(top.state==2)
+            {
+                st.pop();
+            }
+        }
+        return root;
     }
     public static void main(String[] args) {
         
