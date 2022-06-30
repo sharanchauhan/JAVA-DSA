d4899de5ad2441548fccc7ed3e2e3867196af91f
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-30 22:15:03 +0530

    Function to display the Binary Search Tree added to Main.java file

--- BinarySearchTree/L001/Main.java
+++ BinarySearchTree/L001/Main.java
@@ -62,4 +62,19 @@ public class Main {
 
         return root;
     }
+
+    public static void display(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        String str = "";
+        str += node.left == null ? "." : node.left.data + "";
+        str += " <- " + node.data + " -> ";
+        str += node.right == null ? "." : node.right.data + "";
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
+    }
 }
