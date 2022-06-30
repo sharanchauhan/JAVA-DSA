d1132849ec25efea8414166a9585b0e40105457c
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-06-30 22:11:09 +0530

    Structure Added

---
+++ BinarySearchTree/L001/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+}
