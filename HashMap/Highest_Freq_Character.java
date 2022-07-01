348d62f1679c2eb1e2e36d5294f94363cfdaf21e
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-07-01 22:33:18 +0530

    Highest Frequency Character Done

---
+++ HashMap/Highest_Freq_Character.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Highest_Freq_Character {
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        HashMap<Character,Integer> hm=new HashMap<>();
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        for(int i=0;i<str.length();i++)
+        {
+            if(hm.containsKey(str.charAt(i)))
+            {
+                hm.put(str.charAt(i),(hm.get(str.charAt(i))+1));
+            }
+            else
+            {
+                hm.put(str.charAt(i),1);
+            }
+        }
+        int maxFreq=0;
+        char maxFch=' ';
+        for(Character ch:hm.keySet())
+        {
+            if(maxFreq<hm.get(ch))
+            {
+                maxFreq=hm.get(ch);
+                maxFch=ch;
+            }
+        }
+        System.out.println(maxFch);
+    }
+}
