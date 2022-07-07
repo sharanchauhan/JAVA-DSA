7e8d4e0770611fbe66d1dcffd48fde71a9d02d45
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-07-07 18:27:28 +0530

    Copied code removed and done by self

--- Graph/L001/Main.java
+++ Graph/L001/Main.java
@@ -1,37 +1,41 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static class Edge {
-        int src, nbr;
-
-        Edge(int src, int nbr) {
-            this.src = src;
-            this.nbr = nbr;
+    public static class Edge
+    {
+        int src,nbr;
+        Edge(int src,int nbr)
+        {
+            this.src=src;
+            this.nbr=nbr;
         }
     }
-
-    public static void main(String args[]) {
-        Scanner scn = new Scanner(System.in);
-        int nvtces = scn.nextInt();
-        ArrayList<Edge>[] graph = new ArrayList[nvtces];
-        for (int i = 0; i < nvtces; i++) {
-            graph[i] = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int nvtces=s.nextInt();
+        ArrayList<Edge>[] graph=new ArrayList[nvtces];
+        for(int i=0;i<nvtces;i++)
+        {
+            graph[i]=new ArrayList<>();
         }
-        int nedges = scn.nextInt();
+        int nedges=s.nextInt();
         for (int i = 1; i <= nedges; i++) {
-            int v1 = scn.nextInt(), v2 = scn.nextInt();
+            int v1=s.nextInt();
+            int v2=s.nextInt();
             graph[v1].add(new Edge(v1, v2));
             graph[v2].add(new Edge(v2, v1));
         }
 
         for (ArrayList<Edge> list : graph) {
             for (Edge e : list) {
-                System.out.println(e.src + "->" + e.nbr);
+                System.out.println(e.src+"->"+e.nbr);
             }
         }
-        scn.close();
+        s.close();
     }
 }
+
 
 /*
 7
