ef047845ae5e6d44891c83cc0d4635fee36cf33e
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-07-07 18:55:00 +0530

    Recursive function which checks whether a path exists between source and destination

--- Graph/L001/Main.java
+++ Graph/L001/Main.java
@@ -2,50 +2,64 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static class Edge
-    {
-        int src,nbr;
-        Edge(int src,int nbr)
-        {
-            this.src=src;
-            this.nbr=nbr;
+    public static class Edge {
+        int src, nbr;
+
+        Edge(int src, int nbr) {
+            this.src = src;
+            this.nbr = nbr;
         }
     }
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int nvtces=s.nextInt();
-        ArrayList<Edge>[] graph=new ArrayList[nvtces];
-        for(int i=0;i<nvtces;i++)
-        {
-            graph[i]=new ArrayList<>();
+
+    public static boolean hasPath(ArrayList<Edge>[] graph, int vtx, int dest, boolean vis[]) {
+        if (vtx == dest) {
+            return true;
         }
-        int nedges=s.nextInt();
+        vis[vtx] = true;
+        for (Edge e : graph[vtx]) {
+            if (vis[e.nbr] == false) {
+                boolean rres = hasPath(graph, e.nbr, dest, vis);
+                if (rres) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int nvtces = s.nextInt();
+        ArrayList<Edge>[] graph = new ArrayList[nvtces];
+        for (int i = 0; i < nvtces; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        int nedges = s.nextInt();
         for (int i = 1; i <= nedges; i++) {
-            int v1=s.nextInt();
-            int v2=s.nextInt();
+            int v1 = s.nextInt();
+            int v2 = s.nextInt();
             graph[v1].add(new Edge(v1, v2));
             graph[v2].add(new Edge(v2, v1));
         }
 
         for (ArrayList<Edge> list : graph) {
             for (Edge e : list) {
-                System.out.println(e.src+"->"+e.nbr);
+                System.out.println(e.src + "->" + e.nbr);
             }
         }
         s.close();
     }
 }
 
-
 /*
-7
-8
-0 1
-1 2
-2 3
-0 3
-3 4
-4 5
-5 6
-4 6
-*/
+ * 7
+ * 8
+ * 0 1
+ * 1 2
+ * 2 3
+ * 0 3
+ * 3 4
+ * 4 5
+ * 5 6
+ * 4 6
+ */
