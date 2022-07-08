ff7b529d06e651940910a25e5a413248e3a05589
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-07-08 17:13:15 +0530

    Recursive function to print the right view of the Binary Tree added to Main.java file

--- BinaryTree/L002/Main.java
+++ BinaryTree/L002/Main.java
@@ -128,4 +128,25 @@ public class Main {
       }
       return ans;
     }
+
+    int max_level=0;
+    ArrayList<Integer> rightView(Node root) 
+    {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+		rightViewUtil(result, root, 1);
+		return result;
+    }
+    void rightViewUtil(ArrayList<Integer> result, Node node, int level)
+    {
+        if (node==null) return;
+        
+        if (max_level < level)
+        {
+            result.add(node.data);
+            max_level = level;
+        }
+        
+        rightViewUtil(result, node.right, level+1);
+        rightViewUtil(result, node.left, level+1);
+    }
 }
