94a15280a78aff88dd07e4ae66e35db86f6ad7e9
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-07-15 22:36:06 +0530

    Function for fibonacci using memoization added to Main.java file

--- DynamicProgramming/L001/Main.java
+++ DynamicProgramming/L001/Main.java
@@ -9,11 +9,23 @@ public class Main {
         return ans;
     }
 
+    public static int fib_mem(int n, int[] mem) {
+        if (n == 0 || n == 1) {
+            return mem[n] = n;
+        }
+        if (mem[n] != 0) { // already calculated
+            return mem[n];
+        }
+        int ans = fib_mem(n - 1, mem) + fib_mem(n - 2, mem);
+        return mem[n] = ans;
+    }
+
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
         System.out.println(fib_rec(n));
+        System.out.println(fib_mem(n, new int[n + 1]));
         scn.close();
     }
 }
